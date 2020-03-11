b569660ac748427c6903abb274e90644a5de297e
Mangals30 <mangalam301185@gmail.com>
2020-03-11 15:34:04 +0200

    First Commit

--- src/components/AddPost.js
+++ src/components/AddPost.js
@@ -1,16 +1,35 @@
 import React from 'react'
-import Add from './Add'
+import { Link } from 'react-router-dom';
 
-const AddPost = (props) => {
-    const {post,addPost,handleChange} = props
-    
+const AddPost = ({post,addPost,handleChange}) => {
+    const {title,category,description} = post
     return (
-        <div>
-            <Add post = {post}  addPost = {addPost} handleChange = {handleChange}/>
+        <div className = "post-container">
+            <div>
+                <h1>New Post</h1>
+            </div>
+            <div className="form-inner">
+                 <div className="input-holder">
+                    <span className="label"> Title:</span>
+                    <input  className = "input-text" type = "text" name = 'title' value = {title} onChange = {handleChange}></input> 
+                </div> 
+                <div className="input-holder">
+                    <span className="label"> Category:</span>
+                    <input  className = "input-text" type = "text" name = 'category' value = {category} onChange = {handleChange}></input> 
+                </div> 
+                <div className = "input-holder">
+                    <span className="label"> Write new post:</span> 
+                    <textarea className="input-text" type = "text" name = 'description' value = {description} onChange = {handleChange}></textarea>
+                </div>
+                <div>
+                    <Link to='/'><button className="postBtn" onClick = {addPost}>Add</button></Link>
+                    <Link to='/'><button className ="cancelBtn">Cancel</button></Link>
+                </div>
+            </div>        
         </div>
-       
     )
 }
 
 export default AddPost
-  
+
+
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Delete from './Delete';
+
 
 
 const Post = (props) => {
     const {id,title,category} = props.post
-    const {deletePost} = props
+
     return (        
         <div className = 'post-container' id='post-item'>
             <div><Link to = {`/post/${id}`}><h1>{title}</h1></Link></div>
             <div className='form-inner'>
-                <Delete id = {id} deletePost = {deletePost}/>
                 <div className='input-holder'>{category}</div>
-                <div className='input-holder'>{id}</div>
             </div>
         </div>
     )
